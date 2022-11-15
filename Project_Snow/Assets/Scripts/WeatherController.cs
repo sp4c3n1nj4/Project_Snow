254653cc62a24a9d27f60fc94762cd5cae68ca47
Paulinus ERHARDT <u2035424@uel.ac.uk>
2022-11-15 16:22:12 +0000

    start of weather controller

---
+++ Project_Snow/Assets/Scripts/WeatherController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
+
+public class WeatherController : MonoBehaviour
+{
+    public float snowDensity;
+    public float snowIntensity;
+
+    public float fogDistance;
+
+    [SerializeField]
+    private Volume volume;
+    [SerializeField]
+    private VolumeComponent volumeComponent;
+
+
+
+    private void Update()
+    {
+        VolumeProfile profile = volume.sharedProfile;
+        if (!profile.TryGet<Fog>(out var fog))
+        {
+            fog = profile.Add<Fog>(false);
+        }
+        fog.Override(volumeComponent, fogDistance);
+    }
+}
